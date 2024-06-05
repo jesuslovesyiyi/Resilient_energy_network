f273233d5510ab6c597a320bca39aaea04878414
Tiger Peng <nytigeryxp@gmail.com>
2024-06-05 19:39:24 -0400

    Modify scraper to allow chart data to render, ensuring no missed data, log scraping history in output directory, avoid pkl to csv conversion

--- data_preparation/india_esmi_scraper.py
+++ data_preparation/india_esmi_scraper.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from datetime import time, tzinfo, timedelta
+from datetime import timedelta
 import urllib
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -8,13 +8,14 @@ import requests
 import pandas as pd
 from tqdm import tqdm
 import re
-import pickle
+from requests_html import HTMLSession
 
 def date2query(date):
     return urllib.parse.quote(date.strftime('%d/%m/%Y'), safe='')
 
 
-def retry_request(url, session, total=4, status_forcelist=[429, 500, 502, 503, 504], **kwargs):
+# Robust request function that automatically retries
+def retry_request(url, session, total=4, status_forcelist=[429, 500, 502, 503, 504], wait_time=0, **kwargs):
     # Make number of requests required
     for _ in range(total):
         try:
@@ -22,13 +23,16 @@ def retry_request(url, session, total=4, status_forcelist=[429, 500, 502, 503, 5
             if response.status_code in status_forcelist:
                 # Retry request 
                 continue
+
+            if wait_time:
+                response.html.render(wait=wait_time)
             return response
         except requests.exceptions.ConnectionError:
             pass
     return None
 
 
-session = requests.Session()
+session = HTMLSession()
 LOGIN_URL = 'http://www.watchyourpower.org/admin/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
@@ -80,7 +84,6 @@ if res.ok:
     text_list = text.split('<option value="">-- Select  --</option>')[1].split('</select>')[0]
     text_list = re.split('<option class="" | >', text_list)
     stationIDs = [elem[6:].replace('\"', '').replace(' ', '+') for elem in text_list if elem.startswith("value=")]
-    print(f'Stations: {stationIDs}, {len(stationIDs)} stations total')
 
     # If the output directory already exists, check the log to see the ids and last scraped dates for all of the stations
     last_scraped = {}
@@ -92,11 +95,11 @@ if res.ok:
         if os.path.exists(f'{INDIA_SCRAPE_DIR}/log.txt'):
             with open(f'{INDIA_SCRAPE_DIR}/log.txt', 'r') as f:
                 print('Previous scraping log found:')
-                print(f'Last read: {f.readline().split(':')[1]}')
+                print(f'Last read: {f.readline().split(":")[1]}')
 
                 for line in f:
-                    _, id, date = f.readline().split(',')
-                    last_scraped[id] = date.strptime('%m/%d/%Y')
+                    title, id, date = line.strip().split(',')
+                    last_scraped[id] = (datetime.strptime(date, '%m/%d/%Y'), title)
     else:
         os.makedirs(INDIA_SCRAPE_DIR)
                 
@@ -114,55 +117,80 @@ if res.ok:
             if empty_chart_match:
                 empty_chart_content = empty_chart_match.group(1)
                 range_start = datetime.strptime(empty_chart_content.split('From ')[1].split(' To ')[0].strip(), '%d/%m/%Y')
-                range_end = datetime.strptime(empty_chart_content.split(' To ')[1].strip(), '%d/%m/%Y')
+                range_end = datetime.strptime(empty_chart_content.split(' To ')[1].strip(), '%d/%m/%Y')\
+                
+                # Ignore data before 2015 due to poor data quality before official program launch
+                if range_start < datetime(year=2015, month=1, day=1):
+                    range_start = datetime(year=2015, month=1, day=1)
+
+            title_match = re.search(r'<h4>[\s]*?Location - (.*?), District - (.*?), State - (.*?) \..*?<\/h4>', res.text)
+            location = title_match.group(1).strip()
+            district = title_match.group(2).strip()
+            state = title_match.group(3).strip()
+            title = re.sub(r'[\s]*-[\s]*', '-', f'{location}+{district}+{state}'.replace('/', '_')).replace(' ', '_')
+            print(title)
         else:
             print(f'Failed to retrieve data for station {id}')
             continue
+
+        # If the station has already been scraped, check the last scraped date and start from there
+        if id in last_scraped:
+            print('station found')
+            last_date = last_scraped[id][0]
+            if last_date >= range_start:
+                range_start = last_date + timedelta(days=1)
+            
+            if range_end < range_start: # If the last scraped date is after the last available date, skip this station
+                print(f'Station {id} already up to date')
+                continue
 
         query_start = range_start
         query_end = query_start + timedelta(days=31)
 
         # Loop through the available data period in 31 day intervals
-        result = []
-        while query_end <= range_end and query_start < query_end:
-            url = f'http://www.watchyourpower.org/reports.php?location_id={id}&from_date={date2query(query_start)}&to_date={date2query(query_end)}'
+        dates = []
+        voltages = []
+        with tqdm(total=(range_end - range_start).days) as pbar:
+            while query_end <= range_end and query_start < query_end:
+                url = f'http://www.watchyourpower.org/reports.php?location_id={id}&from_date={date2query(query_start)}&to_date={date2query(query_end)}'
+                res = retry_request(url=url, session=session, headers=HEADERS, wait_time=0.5)
 
-            res = retry_request(url=url, session=session, headers=HEADERS)
+                if res:
+                    text = res.text
+                    pbar.set_description(f'Scraping {query_start.strftime("%m/%d/%Y")} to {query_end.strftime("%m/%d/%Y")} for station {id}')
 
-            if res:
-                text = res.text
-                title = text.split('<title>')[1].split('for')[0]
+                    # Separate out all of the line chart data points into their "date" : date, "voltage" : voltage pairs, with the brackets removed
+                    test_split = text.split('linechartData = [{')
+                    if len(test_split) < 2:
+                        pbar.set_description(f'No data found for station {id} between {query_start.strftime("%m/%d/%Y")} and {query_end.strftime("%m/%d/%Y")}')
+                    else:
+                        datapoints = test_split[1].split('];')[0].split('},{')
+                        for point in datapoints:
+                            date = datetime.strptime(point.split('"date":"')[1].split('","')[0], '%a %b %d %Y %H:%M:%S')
+                            voltage = int(point.split('"voltage":"')[1].split('"')[0])
 
-                print(title)
-                print(f'Scraping {query_start.strftime('%m/%d/%Y')} to {query_end.strftime('%m/%d/%Y')} for station {id}')
+                            dates.append(date)
+                            voltages.append(voltage)
 
-                text_list = text.split('\n')
-                for elem in text_list:
-                    if elem.startswith("\t\tvar linechartData"):
-                        voltage_list = elem[24:-3].split('},{')
-                        for volt in tqdm(voltage_list):
-                            result.append(eval("{" + volt.replace('null', '\"null\"') + "}"))
-            
-            query_start = query_end + timedelta(days=1)
-            query_end = query_start + timedelta(days=31)
+                pbar.update((query_end - query_start).days + 1)
+                
+                query_start = query_end + timedelta(days=1)
+                query_end = query_start + timedelta(days=31)
 
-            if query_end > range_end:
-                query_end = range_end
-
-        print(len(result))
-
-        if os.path.exists(f'{INDIA_SCRAPE_DIR}/{id}.pkl'):
-            continue
+                if query_end > range_end:
+                    query_end = range_end
 
         # Update last scraped
-        last_scraped[id] = range_end
+        last_scraped[id] = (range_end, title)
 
-        save = {'name': title, 'timeseries': result}
-        with open(f'{INDIA_SCRAPE_DIR}/{id}.pkl', 'wb') as f:
-            pickle.dump(save, f)
+        print(f'Total entries: {len(dates)}')
+        df = pd.DataFrame({'time': dates, 'voltage': voltages})
+        df.to_csv(f'{INDIA_SCRAPE_DIR}/{id}+{title}.csv', index=False)
     
     # Update the log file
     with open(f'{INDIA_SCRAPE_DIR}/log.txt', 'w') as f:
         f.write(f'Last Scraped:{today_string}\n')
-        for id, date in last_scraped.items():
-            f.write(f'{title},{id},{date.strftime('%m/%d/%Y')}\n')
+        for id, info in last_scraped.items():
+            date = info[0]
+            title = info[1]
+            f.write(f'{title},{id},{date.strftime("%m/%d/%Y")}\n')
