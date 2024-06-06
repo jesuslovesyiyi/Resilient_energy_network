ef4b2b72486f87022d9c533143909a6106efe206
Tiger Peng <nytigeryxp@gmail.com>
2024-06-06 14:58:45 -0400

    Remove reliance on unreliable requests-html, verify data strictly greater than previous scraping output except for pre-2015

--- data_preparation/india_esmi_scraper.py
+++ data_preparation/india_esmi_scraper.py
@@ -8,14 +8,13 @@ import requests
 import pandas as pd
 from tqdm import tqdm
 import re
-from requests_html import HTMLSession
 
 def date2query(date):
     return urllib.parse.quote(date.strftime('%d/%m/%Y'), safe='')
 
 
 # Robust request function that automatically retries
-def retry_request(url, session, total=4, status_forcelist=[429, 500, 502, 503, 504], wait_time=0, **kwargs):
+def retry_request(url, session, total=4, status_forcelist=[429, 500, 502, 503, 504], **kwargs):
     # Make number of requests required
     for _ in range(total):
         try:
@@ -24,15 +23,13 @@ def retry_request(url, session, total=4, status_forcelist=[429, 500, 502, 503, 5
                 # Retry request 
                 continue
 
-            if wait_time:
-                response.html.render(wait=wait_time)
             return response
         except requests.exceptions.ConnectionError:
             pass
     return None
 
 
-session = HTMLSession()
+session = requests.Session()
 LOGIN_URL = 'http://www.watchyourpower.org/admin/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
@@ -102,95 +99,105 @@ if res.ok:
                     last_scraped[id] = (datetime.strptime(date, '%m/%d/%Y'), title)
     else:
         os.makedirs(INDIA_SCRAPE_DIR)
-                
-    for id in stationIDs:
-        # Query just the location with no dates to pull up the error text which informs us of the available data dates
-        url = f'http://www.watchyourpower.org/reports.php?location_id={id}'
-        res = retry_request(url=url, session=session, headers=HEADERS)
 
-        range_start = None
-        range_end = None
-        title = ""
+    try:
+        for id in stationIDs:
+            # Query just the location with no dates to pull up the error text which informs us of the available data dates
+            url = f'http://www.watchyourpower.org/reports.php?location_id={id}'
+            res = retry_request(url=url, session=session, headers=HEADERS)
 
-        if res:
-            empty_chart_match = re.search(r'<span class="empty_chart">([\S\s]*?)<\/span>', res.text)
-            if empty_chart_match:
-                empty_chart_content = empty_chart_match.group(1)
-                range_start = datetime.strptime(empty_chart_content.split('From ')[1].split(' To ')[0].strip(), '%d/%m/%Y')
-                range_end = datetime.strptime(empty_chart_content.split(' To ')[1].strip(), '%d/%m/%Y')\
-                
-                # Ignore data before 2015 due to poor data quality before official program launch
-                if range_start < datetime(year=2015, month=1, day=1):
-                    range_start = datetime(year=2015, month=1, day=1)
+            range_start = None
+            range_end = None
+            title = ""
 
-            title_match = re.search(r'<h4>[\s]*?Location - (.*?), District - (.*?), State - (.*?) \..*?<\/h4>', res.text)
-            location = title_match.group(1).strip()
-            district = title_match.group(2).strip()
-            state = title_match.group(3).strip()
-            title = re.sub(r'[\s]*-[\s]*', '-', f'{location}+{district}+{state}'.replace('/', '_')).replace(' ', '_')
-            print(title)
-        else:
-            print(f'Failed to retrieve data for station {id}')
-            continue
+            if res:
+                empty_chart_match = re.search(r'<span class="empty_chart">([\S\s]*?)<\/span>', res.text)
+                if empty_chart_match:
+                    empty_chart_content = empty_chart_match.group(1)
+                    range_start = datetime.strptime(empty_chart_content.split('From ')[1].split(' To ')[0].strip(), '%d/%m/%Y')
+                    range_end = datetime.strptime(empty_chart_content.split(' To ')[1].strip(), '%d/%m/%Y')
+                    
+                    # Ignore data before 2015 due to poor data quality before official program launch
+                    if range_start < datetime(year=2015, month=1, day=1):
+                        range_start = datetime(year=2015, month=1, day=1)
 
-        # If the station has already been scraped, check the last scraped date and start from there
-        if id in last_scraped:
-            print('station found')
-            last_date = last_scraped[id][0]
-            if last_date >= range_start:
-                range_start = last_date + timedelta(days=1)
-            
-            if range_end < range_start: # If the last scraped date is after the last available date, skip this station
-                print(f'Station {id} already up to date')
+                title_match = re.search(r'<h4>[\s]*?Location - (.*?), District - (.*?), State - (.*?) \..*?<\/h4>', res.text)
+                location = title_match.group(1).strip()
+                district = title_match.group(2).strip()
+                state = title_match.group(3).strip()
+                title = re.sub(r'[\s]*-[\s]*', '-', f'{location}+{district}+{state}'.replace('/', '_')).replace(' ', '_')
+                print(title)
+            else:
+                print(f'Failed to retrieve data for station {id}')
                 continue
 
-        query_start = range_start
-        query_end = query_start + timedelta(days=31)
-
-        # Loop through the available data period in 31 day intervals
-        dates = []
-        voltages = []
-        with tqdm(total=(range_end - range_start).days) as pbar:
-            while query_end <= range_end and query_start < query_end:
-                url = f'http://www.watchyourpower.org/reports.php?location_id={id}&from_date={date2query(query_start)}&to_date={date2query(query_end)}'
-                res = retry_request(url=url, session=session, headers=HEADERS, wait_time=0.5)
-
-                if res:
-                    text = res.text
-                    pbar.set_description(f'Scraping {query_start.strftime("%m/%d/%Y")} to {query_end.strftime("%m/%d/%Y")} for station {id}')
-
-                    # Separate out all of the line chart data points into their "date" : date, "voltage" : voltage pairs, with the brackets removed
-                    test_split = text.split('linechartData = [{')
-                    if len(test_split) < 2:
-                        pbar.set_description(f'No data found for station {id} between {query_start.strftime("%m/%d/%Y")} and {query_end.strftime("%m/%d/%Y")}')
-                    else:
-                        datapoints = test_split[1].split('];')[0].split('},{')
-                        for point in datapoints:
-                            date = datetime.strptime(point.split('"date":"')[1].split('","')[0], '%a %b %d %Y %H:%M:%S')
-                            voltage = int(point.split('"voltage":"')[1].split('"')[0])
-
-                            dates.append(date)
-                            voltages.append(voltage)
-
-                pbar.update((query_end - query_start).days + 1)
+            # If the station has already been scraped, check the last scraped date and start from there
+            if id in last_scraped:
+                print('station found')
+                last_date = last_scraped[id][0]
+                if last_date >= range_start:
+                    range_start = last_date + timedelta(days=1)
                 
-                query_start = query_end + timedelta(days=1)
-                query_end = query_start + timedelta(days=31)
+                if range_end < range_start: # If the last scraped date is after the last available date, skip this station
+                    print(f'Station {id} already up to date')
+                    continue
 
-                if query_end > range_end:
-                    query_end = range_end
+            query_start = range_start
+            query_end = query_start + timedelta(days=31)
 
-        # Update last scraped
-        last_scraped[id] = (range_end, title)
+            # Loop through the available data period in 31 day intervals
+            dates = []
+            voltages = []
+            with tqdm(total=(range_end - range_start).days) as pbar:
+                while query_end <= range_end and query_start < query_end:
+                    url = f'http://www.watchyourpower.org/reports.php?location_id={id}&from_date={date2query(query_start)}&to_date={date2query(query_end)}'
+                    res = retry_request(url=url, session=session, headers=HEADERS)
 
-        print(f'Total entries: {len(dates)}')
-        df = pd.DataFrame({'time': dates, 'voltage': voltages})
-        df.to_csv(f'{INDIA_SCRAPE_DIR}/{id}+{title}.csv', index=False)
+                    if res:
+                        text = res.text
+                        pbar.set_description(f'Scraping {query_start.strftime("%m/%d/%Y")} to {query_end.strftime("%m/%d/%Y")} for station {id}')
+
+                        # Separate out all of the line chart data points into their "date" : date, "voltage" : voltage pairs, with the brackets removed
+                        test_split = text.split('linechartData = [{')
+                        if len(test_split) < 2:
+                            pbar.set_description(f'No data found for station {id} between {query_start.strftime("%m/%d/%Y")} and {query_end.strftime("%m/%d/%Y")}')
+                            # If the data is not empty, which would present the empty_chart span, save the html for manual inspection
+                            if not re.search(r'<span class="empty_chart">([\S\s]*?)<\/span>', res.text):
+                                with open(f'{INDIA_SCRAPE_DIR}/error_{id}_{query_start.strftime("%m-%d-%Y")}.html', 'w') as f:
+                                    f.write(text)
+                        else:
+                            datapoints = test_split[1].split('];')[0].split('},{')
+                            for point in datapoints:
+                                date = datetime.strptime(point.split('"date":"')[1].split('","')[0], '%a %b %d %Y %H:%M:%S')
+                                voltage = int(point.split('"voltage":"')[1].split('"')[0])
+
+                                dates.append(date)
+                                voltages.append(voltage)
+
+                    pbar.update((query_end - query_start).days + 1)
+                    
+                    query_start = query_end + timedelta(days=1)
+                    query_end = query_start + timedelta(days=31)
+
+                    if query_end > range_end:
+                        query_end = range_end
+
+            # Update last scraped
+            last_scraped[id] = (range_end, title)
+
+            print(f'Total entries: {len(dates)}')
+            df = pd.DataFrame({'time': dates, 'voltage': voltages})
+            if id in last_scraped: # Update old file if it exists
+                df_old = pd.read_csv(f'{INDIA_SCRAPE_DIR}/{id}+{title}.csv')
+                df = pd.concat([df_old, df], ignore_index=True)
+            
+            df.to_csv(f'{INDIA_SCRAPE_DIR}/{id}+{title}.csv', index=False)
     
     # Update the log file
-    with open(f'{INDIA_SCRAPE_DIR}/log.txt', 'w') as f:
-        f.write(f'Last Scraped:{today_string}\n')
-        for id, info in last_scraped.items():
-            date = info[0]
-            title = info[1]
-            f.write(f'{title},{id},{date.strftime("%m/%d/%Y")}\n')
+    finally:
+        with open(f'{INDIA_SCRAPE_DIR}/log.txt', 'w') as f:
+            f.write(f'Last Scraped:{today_string}\n')
+            for id, info in last_scraped.items():
+                date = info[0]
+                title = info[1]
+                f.write(f'{title},{id},{date.strftime("%m/%d/%Y")}\n')
