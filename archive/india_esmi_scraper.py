2cb05c9e8efb58194781075fa8efa4bb05d31705
Yiyi He <yiyi_he@berkeley.edu>
2025-08-19 14:46:30 -0400

    organization

--- archive/india_esmi_scraper.py
+++ archive/india_esmi_scraper.py
@@ -9,6 +9,13 @@ import pandas as pd
 from tqdm import tqdm
 import re
 
+
+# india_esmi_scraper.py: scrapes the ESMI website to get available data for currently online stations, resuming where the last scraping session had left off.
+# Ignores data before January 1st, 2015, because that was when Prayas officially launched the ESMI program, and the available data quality before that is flawed.
+# Outputs csv files in the format [id]+[location]+[district]+[state].csv
+# The minute by minute datetimes are recorded in the default pandas export format yyyy-MM-dd HH:mm:ss.
+# indonesia_date_conversion.py: converts dates in the indonesia dataset to the uniform date format used throughout this project- india_pkl2csv.py: [Deprecated] converted previous pkl output of the india scraper to the final csv format.
+
 def date2query(date):
     return urllib.parse.quote(date.strftime('%d/%m/%Y'), safe='')
 
